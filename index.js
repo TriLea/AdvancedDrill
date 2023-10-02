430838aa0bb50fd9b0cee93b5f53b0efd4e6ab40
tristan <tlea5@uw.com>
2023-10-01 18:05:10 -0700

    setup of form, nav, and css

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-let slideIndex = 0;
+// let slideIndex = 0;
 
-function showSlides() {
-    const slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+// function showSlides() {
+//     const slides = document.getElementsByClassName("slide");
+
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) { slideIndex = 1 }
+//     slides[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
+
+// showSlides();
+
+document.getElementById('requestQuoteForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var details = document.getElementById('details').value;
+    var errorMessage = document.getElementById('error-message');
+
+    if (name === "" || email === "" || phone === "" || details === "") {
+        errorMessage.textContent = "All fields are required!";
+    } else {
+        errorMessage.textContent = "";
+        // TODO: Implement the code to send the form data to a server or handle accordingly.
+        alert('Quote requested successfully!');
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+});
 
-showSlides();
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 
-// const parallaxImage = document.querySelector('.parallax-bg');
-
-// window.addEventListener('scroll', function() {
-//     let offset = window.scrollY;
-//     parallaxImage.style.top = offset * 0.5 + 'px'; 
-// });
+hamburger.addEventListener('click', function () {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
